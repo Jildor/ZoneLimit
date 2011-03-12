93595af82031c6d5fc936957fdbd5c74391fe502
Jildor <gildor55@gmail.com>
2011-03-12 12:06:28 +0100

    Core/Commands: Allow npc info to show both DB GUID and current GUID

---
+++ sql/updates/world/2011_03_11_2_trinity_string.sql
@@ -0,0 +1,14 @@
+UPDATE `trinity_string` SET `content_default` = 'Player selected NPC
+DB GUID: %u, current GUID: %u.
+Faction: %u.
+npcFlags: %u.
+Entry: %u.
+DisplayID: %u (Native: %u).',
+-- Editado por Gildor
+-- Lo pongo en español también.
+content_loc6 = 'Jugador Seleccionado PNJ
+DB GUID: %u, GUID actual: %u.
+Facción: %u.
+npcFlags: %u.
+Entrada: %u.
+DisplayID: %u (Nativo: %u).' WHERE `entry` = 539;
