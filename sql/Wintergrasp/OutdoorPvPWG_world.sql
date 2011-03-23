796f2d499ff32ba79fa493f475ea4e6157a88b24
Jildor <gildor55@gmail.com>
2011-03-23 17:43:00 +0100

    Error otro Guid...xD

--- sql/Wintergrasp/OutdoorPvPWG_world.sql
+++ sql/Wintergrasp/OutdoorPvPWG_world.sql
@@ -137,7 +137,7 @@ DELETE FROM `gameobject` WHERE `id`=193772;
 INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
 ('44149', 193772, 571, 1, 64, 5924.11, 573.387, 661.087, 4.43208, 0, 0, 0.798953, -0.601393, 300, 0, 1),
 ('44120', 193772, 571, 1, 65535, 5686.57, 772.921, 647.754, 5.62225, 0, 0, 0.324484, -0.945891, 600, 0, 1),
-('44177', 193772, 571, 1, 65535, 5930.82, 548.961, 640.632, 1.88506, 0, 0, 0.809047, 0.587744, 300, 0, 1);
+('44115', 193772, 571, 1, 65535, 5930.82, 548.961, 640.632, 1.88506, 0, 0, 0.809047, 0.587744, 300, 0, 1);
 
 -- Guids para CTDB
 /* Wintergrasp Battle-Mage */
