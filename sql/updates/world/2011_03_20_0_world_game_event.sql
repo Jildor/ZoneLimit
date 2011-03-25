72785691d147ebcbd587a424be5107015fe6b8cf
Jildor <gildor55@gmail.com>
2011-03-25 17:46:24 +0100

    Core/Battlegrounds: Fixed displaying Call to Arms for Isle of Conquest and Strand of the Ancients

---
+++ sql/updates/world/2011_03_20_0_world_game_event.sql
@@ -0,0 +1,8 @@
+ -- Editado por Gildor
+ -- Cambiados entrys por los de CTDB
+ -- Actualizados datos igual que en TDB
+
+ -- Call to Arms: Strand of the Ancients!
+UPDATE `game_event` SET `start_time` = '2010-04-09 08:00:00', `end_time` = '2020-12-31 10:00:00', `length` = 6240, `holiday`=400 WHERE `eventEntry`=30;
+ -- Call to Arms: Isle of Conquest!
+UPDATE `game_event` SET `start_time` = '2010-04-16 08:00:00', `end_time` = '2020-12-31 10:00:00', `length` = 6240, `holiday`=420 WHERE `eventEntry`=42;
