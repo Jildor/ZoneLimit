e7cdaeb864f6ab2f412262586bec9688b48e7486
Jildor <gildor55@gmail.com>
2011-03-26 21:32:45 +0100

    DB - Kill startup errors

---
+++ sql/updates/world/2011_03_23_12_world_misc.sql
@@ -0,0 +1,10 @@
+ -- Editado por Gildor
+ -- DB Startup errors
+DELETE FROM `conditions` WHERE `SourceEntry`=43151 AND `SourceTypeOrReferenceId`=13; -- Delete uneed/wrong conditions. Assigned SAI will take care of targeting the right creature
+ -- Delete wrong equip template and revert the one that shouldn't have been changed
+UPDATE `creature_template` SET `equipment_id`=2431 WHERE `entry`=18697;
+ -- Le quito uno duplicado que no se usa, y le añado los dos de TDB
+DELETE FROM `creature_equip_template` WHERE `entry` IN (2148,1807,2431);
+INSERT INTO `creature_equip_template` (`entry`,`equipentry1`,`equipentry2`,`equipentry3`) VALUES
+(1807,50757,0,0),
+(2431,31301,0,0);
