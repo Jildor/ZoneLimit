22f0d81238f73b3c3f456a4d20f37c99f05a9bf5
Jildor <gildor55@gmail.com>
2011-03-26 19:17:36 +0100

    Se me ha olvidao

--- sql/updates/world/toc5_DK.sql
+++ sql/updates/world/toc5_DK.sql
@@ -1,3 +1,4 @@
 UPDATE `creature_template` SET `npcflag`=`npcflag`|16777216 WHERE `entry` IN (35491);
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` = 35491;
 INSERT INTO `npc_spellclick_spells` (`npc_entry`,`spell_id`,`quest_start`,`quest_start_active`,`quest_end`,`cast_flags`,`aura_required`,`aura_forbidden`,`user_type`) VALUES
 (35491,47020,0,0,0,1,0,0,0);
