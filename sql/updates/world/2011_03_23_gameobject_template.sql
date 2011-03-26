b135ad520fcab54014b816400a3b1c6f5c015cf9
Jildor <gildor55@gmail.com>
2011-03-26 18:03:08 +0100

    DB/Quest: Call of Water (9504)

---
+++ sql/updates/world/2011_03_23_gameobject_template.sql
@@ -0,0 +1,5 @@
+-- Editado por Gildor
+-- NEEDS VISUAL (apart form Ruins of Stardust, its MISSING!)
+-- En CTDB ya esta spawneado, solo le cambio el ID y el Guid
+UPDATE `gameobject` SET `id` = 202275 WHERE guid = 1943;
+
