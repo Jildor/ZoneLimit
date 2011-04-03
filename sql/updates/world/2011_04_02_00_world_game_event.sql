b2e177cba4003a3ba3e9ed0d99de734c633bc0e1
Jildor <gildor55@gmail.com>
2011-04-03 11:36:43 +0200

    mesquivocao

--- sql/updates/world/2011_04_02_00_world_game_event.sql
+++ sql/updates/world/2011_04_02_00_world_game_event.sql
@@ -1,3 +1,3 @@
 -- Editado por Gildor
 -- Le pongo el entry de CTDB y lo dejo como en TDB
-UPDATE `game_event` SET `start_time`= '2008-01-02 21:00:00', end_time '2020-12-31 06:00:00' WHERE `eventEntry`=29;
+UPDATE `game_event` SET `start_time`= '2008-01-02 21:00:00', `end_time` = '2020-12-31 06:00:00' WHERE `eventEntry`=29;
