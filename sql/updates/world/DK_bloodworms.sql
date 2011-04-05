1360ac8c0610fe6e4b0138d4211dfd40deabea47
Jildor <gildor55@gmail.com>
2011-04-05 10:51:52 +0200

    Fixed Bloodworms talent of Death Knights

---
+++ sql/updates/world/DK_bloodworms.sql
@@ -0,0 +1,4 @@
+-- Bloodworm AI
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = 28017;
+INSERT INTO `creature_ai_scripts` VALUES 
+(2801700, 28017, 4, 0, 100, 0, 0, 0, 0, 0, 11, 50453, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodworm - Health Leech');
