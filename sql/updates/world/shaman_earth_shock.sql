04dc28138a6f47f643802374c9919da699ca66d3
Jildor <gildor55@gmail.com>
2011-05-02 22:07:36 +0200

    SQL/Spells: Fixed stacking behavior for spell Earth Shock and other attack speed decreasing effects. Also restored rule for druid's talent Infected Wounds.

---
+++ sql/updates/world/shaman_earth_shock.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_group` WHERE `spell_id` IN (8042,-2009);
+INSERT INTO `spell_group` VALUES (2009,8042),(1064,-2009);
+UPDATE `spell_group` SET `spell_id`=58179 WHERE `spell_id`=48483;
