315e2b8a1c168a24541889534eac3397c7bc86ef
Jildor <gildor55@gmail.com>
2011-05-18 17:31:36 +0200

    SQL/Spells: Quito este sql, ya esta puesto por TC.

--- sql/updates/world/warlock_glyph_of_shadowflame.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_linked_spell` where `spell_trigger` IN (-47960, -61291);
-INSERT INTO `spell_linked_spell` VALUES
-(-47960,-63311, 0, 'Glyph of Shadowflame Rank 1 (Dispel)'),
-(-61291,-63311, 0, 'Glyph of Shadowflame Rank 2 (Dispel)');
