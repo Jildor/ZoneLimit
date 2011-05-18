62684204a15fa5786d06565a35d5206b0c426d19
Jildor <gildor55@gmail.com>
2011-05-18 17:03:56 +0200

    SQL/Spells: Fixed Glyph of Shadowflame. Now it will be properly removed from target if Shadowflame aura is dispelled. Patch by tehmarto

---
+++ sql/updates/world/warlock_glyph_of_shadowflame.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_linked_spell` where `spell_trigger` IN (-47960, -61291);
+INSERT INTO `spell_linked_spell` VALUES
+(-47960,-63311, 0, 'Glyph of Shadowflame Rank 1 (Dispel)'),
+(-61291,-63311, 0, 'Glyph of Shadowflame Rank 2 (Dispel)');
