3571a0104742467cbda4b1efb53d7db3796af71f
Jildor <gildor55@gmail.com>
2011-05-21 18:31:08 +0200

    Core/Spells: Fix talent Scent of Blood

--- sql/updates/world/DK_scent_of_blood.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` IN (49004,49508,49509);
-INSERT INTO `spell_proc_event` (`entry`,`procEx`) VALUES
-(49004,0x00000033),
-(49508,0x00000033),
-(49509,0x00000033);
