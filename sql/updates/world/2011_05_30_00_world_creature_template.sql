841e6ab0b41f022e66190f9e625d6eb4c4260e15
Jildor <gildor55@gmail.com>
2011-06-04 17:20:31 +0200

    Scripts/World: Update WorldBoss scripts for the Emerald Dragons (Ysondre, Emeriss, Taerar and Lethon) + create base "Emerald Dragon" AI (They are still WIP, so do not consider them "workable" unless you are willing to report issues).

---
+++ sql/updates/world/2011_05_30_00_world_creature_template.sql
@@ -0,0 +1,10 @@
+-- Editado por Gildor
+-- Les quito los AIScripts
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='npc_dream_fog' WHERE `entry`=15224;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_ysondre' WHERE `entry`=14887;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='npc_demented_druid' WHERE `entry`=15260;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_ysondre' WHERE `entry`=14887;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_lethon' WHERE `entry`=14888;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_emeriss' WHERE `entry`=14889;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_taerar' WHERE `entry`=14890;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_shade_of_taerar' WHERE `entry`=15302;
