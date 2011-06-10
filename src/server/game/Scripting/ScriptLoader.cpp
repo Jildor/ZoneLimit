df7d2707e7b9807ec564b4cd6e1c61c210bb5acb
Jildor <gildor55@gmail.com>
2011-06-10 17:52:53 +0200

    Ruby Sanctum:
    - Scripted General Zarithrian
    - Flame walls will now open after killing saviana and baltharus
    - Fixed savianas timers
    - Fixed some typos (baltarhus -> baltharus)

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -488,6 +488,7 @@ void AddSC_instance_ruby_sanctum();      // Ruby Sanctum
 void AddSC_ruby_sanctum();
 void AddSC_boss_baltharus_the_warborn();
 void AddSC_boss_saviana_ragefire();
+void AddSC_boss_general_zarithrian();
 
 void AddSC_dalaran();
 void AddSC_borean_tundra();
@@ -1193,6 +1194,7 @@ void AddNorthrendScripts()
     AddSC_ruby_sanctum();
     AddSC_boss_baltharus_the_warborn();
     AddSC_boss_saviana_ragefire();
+    AddSC_boss_general_zarithrian();
 
     AddSC_dalaran();
     AddSC_borean_tundra();
