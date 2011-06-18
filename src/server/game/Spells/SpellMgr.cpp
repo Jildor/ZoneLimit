79ddf7a20cce55207ce99ccd406c9acefd9713d9
Jildor <gildor55@gmail.com>
2011-06-18 16:04:41 +0200

    Core/Spells: Fix more spells that make damage from percent of health

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4140,7 +4140,7 @@ void SpellMgr::LoadSpellCustomAttr()
         case 63024: // Gravity Bomb
         case 64234: // Gravity Bomb (25m)
             spellInfo->MaxAffectedTargets = 1;
-            count++;
+            ++count;
             break;
         case 62834: // Boom
         // This hack is here because we suspect our implementation of spell effect execution on targets
@@ -4397,7 +4397,7 @@ void SpellMgr::LoadEnchantCustomAttr()
                 if (!ench)
                     continue;
                 mEnchantCustomAttr[enchId] = true;
-                count++;
+                ++count;
                 break;
             }
         }
