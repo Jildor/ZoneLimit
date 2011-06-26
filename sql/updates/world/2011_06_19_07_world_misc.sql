0a1eaab369a40e79b3c74e49dc50dae92068d0c4
Jildor <gildor55@gmail.com>
2011-06-26 19:53:43 +0200

    Core/Spells: Fix crash with spellstealing of Divine Illumination

--- sql/updates/world/2011_06_19_07_world_misc.sql
+++ sql/updates/world/2011_06_19_07_world_misc.sql
@@ -1,7 +1,6 @@
 -- Editado por Gildor
 UPDATE `areatrigger_teleport` SET `target_position_x`=-4464.919922,`target_position_y`=-1666.23999,`target_position_z`=81.892769,`target_orientation`=4.288273 WHERE `id`=242;
 UPDATE `game_event` SET `start_time`='2011-09-04 00:01:00' WHERE `eventEntry`=4; -- Darkmoon Faire (Elwynn Forest)
-SET @EquiEntry = 2473; -- (creature_equip_template.entry - need 2)
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`|32768 WHERE `entry`=29333; -- Onslaught Gryphon Rider
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`|32768 WHERE `entry`=29329; -- Onslaught Paladin
 UPDATE `creature_model_info` SET `bounding_radius`=0.3366,`combat_reach`=1.65,`gender`=0 WHERE `modelid`=26179; -- Onslaught Gryphon Rider
