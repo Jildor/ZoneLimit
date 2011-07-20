1bcf8d6969460e030c496acbbd52625dd96a8a50
Jildor <gildor55@gmail.com>
2011-07-20 20:03:55 +0200

    Core/Auras: fix a typo in PowerDrain auras periodic tick handler.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12497,6 +12497,7 @@ int32 Unit::GetHealthGain(int32 dVal)
     return gain;
 }
 
+// returns negative amount on power reduction
 int32 Unit::ModifyPower(Powers power, int32 dVal)
 {
     int32 gain = 0;
