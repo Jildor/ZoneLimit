897033ab5cb6ed34b44ff901cc4e91e4f307849d
Jildor <gildor55@gmail.com>
2011-07-30 21:40:53 +0200

    Revert Fix quest 12983 bad fix.

--- sql/custom/quest_12983.sql
+++
@@ -1,3 +0,0 @@
--- Q 12983
-UPDATE creature_template SET AIName='', ScriptName='npc_injured_icemaw' WHERE entry=29563;
-UPDATE creature_template SET AIName='', ScriptName='npc_harnessed_icemaw' WHERE entry=30468;
