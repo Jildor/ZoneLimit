fd3d32c94db1348f6eb3edcccd43e31aa3f4ce06
Jildor <gildor55@gmail.com>
2011-08-13 11:38:37 +0200

    typo last commit

--- sql/custom/paladin_sacred_shield_cooldown.sql
+++ sql/custom/paladin_sacred_shield_cooldown.sql
@@ -1,3 +1,3 @@
 DELETE FROM `spell_proc_event` WHERE `entry` = 58597;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(58597,0,0,0x00000000,0x00000000,0x00000000,0,0,0 0,6);
+(58597,0,0,0x00000000,0x00000000,0x00000000,0,0,0,0,6);
