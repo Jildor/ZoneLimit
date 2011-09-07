6c7940525c167996ced4afcb2e24bc0acb8212b0
Jildor <gildor55@gmail.com>
2011-09-07 21:57:20 +0200

    Typo

--- sql/custom/paladin_holy_shock_t10.sql
+++ sql/custom/paladin_holy_shock_t10.sql
@@ -1,1 +1,3 @@
-UPDATE `spell_proc_event` SET `SpellFamilyMask1`=0x00010000 WHERE `entry` = 70756;
+DELETE FROM `spell_proc_event` WHERE entry = 70756;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(70756,0x00,10,0x00200000,0,65536,0x00000000,0x00000000,0,0,0);
