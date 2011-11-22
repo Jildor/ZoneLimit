2bcf8faf831984e560dbc55a56003302373a859b
Jildor <gildor55@gmail.com>
2011-11-22 20:52:57 +0100

    Fix achievement The Turkinator works.
    Ths HyD

--- sql/custom/achievement_the_turkinator.sql
+++ sql/custom/achievement_the_turkinator.sql
@@ -4,7 +4,7 @@ SET @SPELL := 62014;
 SET @SOURCETYPE := 0;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
-UPDATE creature_template SET AIName="" WHERE entry=@ENTRY LIMIT 1;
+UPDATE creature_template SET AIName='', ScriptName='npc_wild_turkey' WHERE entry=@ENTRY;
 
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_gen_turkey_tracker';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
