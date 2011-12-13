ac59391b58747db211b7cb1e913f336d4981b1ef
Jildor <gildor55@gmail.com>
2011-12-13 19:20:39 +0100

    forgot

--- sql/custom/achievement_lightning_struck.sql
+++ sql/custom/achievement_lightning_struck.sql
@@ -1,4 +1,8 @@
 -- Editado por Gildor
+-- Le quito los AIScripts
+UPDATE `creature_template` SET `AIName` = '' WHERE entry = 28586;
+DELETE FROM creature_ai_scripts WHERE creature_id = 28586;
+
 -- HoL General Bjarngrim event Temporary Electrical Charge
 -- Clear wrong aura
 UPDATE `creature_addon` SET `auras`='' WHERE `guid` = 115219;
