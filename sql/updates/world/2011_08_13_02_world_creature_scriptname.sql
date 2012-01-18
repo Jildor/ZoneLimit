414addd339b22c8e7e2d5d5a0060b3c380daab51
Jildor <gildor55@gmail.com>
2012-01-18 19:17:04 +0100

    Scripts/Storm Peaks: Make Hyldsmeet Drakerider properly respawn.
    https://github.com/TrinityCore/TrinityCore/commit/0cb68815ec207e519a442283fa6e901ad3704833

---
+++ sql/updates/world/2011_08_13_02_world_creature_scriptname.sql
@@ -0,0 +1,3 @@
+-- Editado por Gildor
+-- Le pongo VehicleId & RegenHealth de TDB
+UPDATE `creature_template` SET `VehicleId`=183,`RegenHealth`=1,`ScriptName`='npc_hyldsmeet_protodrake' WHERE `entry`=29625;
