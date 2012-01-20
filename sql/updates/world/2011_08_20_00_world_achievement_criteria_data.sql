6e5b6a56e4299f3f786ccb118d17246d76ddebd4
Jildor <gildor55@gmail.com>
2012-01-20 19:09:16 +0100

    Scripts/Naxxramas: Fixed achievement Just Can't Get Enough
    https://github.com/TrinityCore/TrinityCore/commit/feb3d9ce209b7368374d6cf10a30f7e528f3ccd7

---
+++ sql/updates/world/2011_08_20_00_world_achievement_criteria_data.sql
@@ -0,0 +1,14 @@
+-- Editado por Gildor
+-- Lo pongo como en TDB
+UPDATE `creature_template` SET `faction_A`=14, `faction_H`=14, `AIName`='', `mechanic_immune_mask`=8388625, `flags_extra`=0 `ScriptName`='npc_kelthuzad_abomination' WHERE `entry` IN (16428,30048);
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=16428;
+
+-- Borro los type 18
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (7614, 7615) AND `type` IN (11,18);
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
+VALUES
+(7614,11,0,0,'achievement_just_cant_get_enough'),
+(7615,11,0,0,'achievement_just_cant_get_enough');
+
+DELETE FROM `disables` WHERE `entry` IN (7614, 7615);
