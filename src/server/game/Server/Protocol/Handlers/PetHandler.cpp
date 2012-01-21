4761ffcf5bd6ddaddaae143aae288e5ee10cee79
Jildor <gildor55@gmail.com>
2012-01-21 10:59:15 +0100

    Core: Clean up here and there. *replase postfix iterator increment\decrement with prefix in cycles *replase strlen(*char) != 0 with *char[0] != '\0' *replase strlen(rStr.c_str()) with rStr.length()
    https://github.com/TrinityCore/TrinityCore/commit/fb2784851099de42c81b10be9820cdb126fc2a56

--- src/server/game/Server/Protocol/Handlers/PetHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/PetHandler.cpp
@@ -678,10 +678,10 @@ void WorldSession::HandlePetAbandon(WorldPacket & recv_data)
             if (pet->GetGUID() == _player->GetPetGUID())
             {
                 uint32 feelty = pet->GetPower(POWER_HAPPINESS);
-                pet->SetPower(POWER_HAPPINESS ,(feelty-50000) > 0 ?(feelty-50000) : 0);
+                pet->SetPower(POWER_HAPPINESS , feelty > 50000 ? (feelty-50000) : 0);
             }
 
-            _player->RemovePet((Pet*)pet,PET_SAVE_AS_DELETED);
+            _player->RemovePet((Pet*)pet, PET_SAVE_AS_DELETED);
         }
         else if (pet->GetGUID() == _player->GetCharmGUID())
             _player->StopCastingCharm();
