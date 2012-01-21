45c544d7518188c4592d8f418709dfdd23c29c85
Jildor <gildor55@gmail.com>
2012-01-21 10:56:13 +0100

    DB/Templates: Added equipment template for Terenas Menethil
    https://github.com/TrinityCore/TrinityCore/commit/7e1f3ceb4435e89a8757e4824700def4b8ecbc12

---
+++ sql/updates/world/2011_08_23_00_world_creature_equip_template.sql
@@ -0,0 +1,5 @@
+-- Editado por Gildor
+-- entry de ZLDB
+DELETE FROM `creature_equip_template` WHERE `entry`=4229;
+INSERT INTO `creature_equip_template` (`entry`,`itemEntry1`,`itemEntry2`,`itemEntry3`) VALUES
+(4229,39659,0,0);
