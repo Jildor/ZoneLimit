b6afd798020061e5d8056f7714839015fa82dc3c
Jildor <gildor55@gmail.com>
2012-01-22 16:30:12 +0100

    Scripts/Vault of Archavon: Earth, Wind & Fire achievement - by idostyle@zoit, with some spell attribute correction to the patch by me.
    https://github.com/TrinityCore/TrinityCore/commit/fab78d5e02ddaab3e4cfeee5ffb9b00f882be248

--- sql/updates/world/2011_XX_XX_XX_world_spell_dbc.sql
+++
@@ -1,6 +0,0 @@
--- Not the right-way™
--- Dummy effect with caster as target
-DELETE FROM `spell_dbc` WHERE `id` IN (68308);
-INSERT INTO `spell_dbc` (`Id`,`Effect1`,`EffectImplicitTargetA1`,`EffectImplicitTargetB1`,`comment`)
-VALUES
-(68308, 3, 1, 0, 'Vault of Archavon - Earth, Wind & Fire - Achievement Check');
