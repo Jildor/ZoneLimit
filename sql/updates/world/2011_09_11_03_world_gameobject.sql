0cdbe30d0c76ebafb2ef264fdd7540015675c014
Jildor <gildor55@gmail.com>
2012-01-23 18:31:19 +0100

    DB/GO: Spawn GO 179545 & 179485 in Dire Maul
    https://github.com/TrinityCore/TrinityCore/commit/25a77fc3672a558d94aafe1b279379dcde49eee3

---
+++ sql/updates/world/2011_09_11_03_world_gameobject.sql
@@ -0,0 +1,7 @@
+-- Editado por Gildor
+UPDATE `gameobject_template` SET `WDBVerified`=12340 WHERE `entry`=179545;
+DELETE FROM `gameobject` WHERE `id` IN (179545,179485);
+-- Le cambio las guids por las de ZLDB
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
+(55201,179545,429,1,1,116.135,638.836,-48.467,-0.785397,0,0,0,1,7200,255,1),
+(55231,179485,429,1,1,558.806,550.065,-25.4008,3.14159,0,0,0,1,180,255,1);
