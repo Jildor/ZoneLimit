c5023e33a3b325ee291785370c841b9d0df8d74e
Jildor <gildor55@gmail.com>
2012-02-03 17:43:10 +0100

    Update char name data when create/delete char.
    https://github.com/TrinityCore/TrinityCore/commit/45de1f01e014f4f473ccd7a4e8d54d61183da1d0

--- src/server/game/Server/Protocol/Handlers/CharacterHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/CharacterHandler.cpp
@@ -689,6 +689,7 @@ void WorldSession::HandleCharCreateCallback(PreparedQueryResult result, Characte
             sLog->outDetail("Account: %d (IP: %s) Create Character:[%s] (GUID: %u)", GetAccountId(), IP_str.c_str(), createInfo->Name.c_str(), newChar.GetGUIDLow());
             sLog->outChar("Account: %d (IP: %s) Create Character:[%s] (GUID: %u)", GetAccountId(), IP_str.c_str(), createInfo->Name.c_str(), newChar.GetGUIDLow());
             sScriptMgr->OnPlayerCreate(&newChar);
+            sWorld->AddCharacterNameData(newChar.GetGUIDLow(), std::string(newChar.GetName()), newChar.getGender(), newChar.getRace(), newChar.getClass());
 
             delete createInfo;
             _charCreateCallback.SetParam(NULL);
@@ -744,6 +745,7 @@ void WorldSession::HandleCharDeleteOpcode(WorldPacket & recv_data)
     sLog->outDetail("Account: %d (IP: %s) Delete Character:[%s] (GUID: %u)", GetAccountId(), IP_str.c_str(), name.c_str(), GUID_LOPART(guid));
     sLog->outChar("Account: %d (IP: %s) Delete Character:[%s] (GUID: %u)", GetAccountId(), IP_str.c_str(), name.c_str(), GUID_LOPART(guid));
     sScriptMgr->OnPlayerDelete(guid);
+    sWorld->DeleteCharaceterNameData(guid);
 
     if (sLog->IsOutCharDump())                                // optimize GetPlayerDump call
     {
