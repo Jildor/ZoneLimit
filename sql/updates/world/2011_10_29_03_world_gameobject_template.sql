7aac72e09ff962a6216721baa8af54f4f42e9e83
Jildor <gildor55@gmail.com>
2012-02-06 12:15:30 +0100

    DB/Templates: Correct 25 man loot IDs for Freya's chest based on the only actual parsed 25man chest there is. NOTE: This is not WDB data, merely guesswork.
    https://github.com/TrinityCore/TrinityCore/commit/cb52cf011fa59a838c2e050e5084efb1fd98b4f4

---
+++ sql/updates/world/2011_10_29_03_world_gameobject_template.sql
@@ -0,0 +1,7 @@
+-- Editado por Gildor
+-- 25 man chest loot IDs
+/* No es necesario (habria que cambiar ids de loot de gameobject)
+UPDATE `gameobject_template` SET `data1`=26961 WHERE `entry`=194330;
+UPDATE `gameobject_template` SET `data1`=26960 WHERE `entry`=194329;
+UPDATE `gameobject_template` SET `data1`=26959 WHERE `entry`=194328;
+*/
