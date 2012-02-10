1143ee74608b4aa41bfbd94f2660d46f82e77941
Jildor <gildor55@gmail.com>
2012-02-10 18:41:31 +0100

    Core/Spells: Since 3.2.2, Earthen Power should no longer provide a snare immunity aura on every pulse. Thanks @kaelima.
    https://github.com/TrinityCore/TrinityCore/commit/f35c8d69f36827334776aea7edb93e07cac8b5bf

--- src/server/scripts/Spells/spell_shaman.cpp
+++ src/server/scripts/Spells/spell_shaman.cpp
@@ -36,7 +36,6 @@ enum ShamanSpells
     // For Earthen Power
     SHAMAN_TOTEM_SPELL_EARTHBIND_TOTEM     = 6474,
     SHAMAN_TOTEM_SPELL_EARTHEN_POWER       = 59566,
-    SHAMAN_TOTEM_SPELL_EARTHEN_POWER_BUFF  = 63532,
 };
 
 // this a dirty hack
@@ -256,22 +255,11 @@ class spell_sha_earthbind_totem : public SpellScriptLoader
             {
                 Unit* target = GetTarget();
                 if (Unit* caster = aurEff->GetBase()->GetCaster())
-                {
                     if (TempSummon* summon = caster->ToTempSummon())
-                    {
                         if (Unit* owner = summon->GetOwner())
-                        {
                             if (AuraEffect* aur = owner->GetDummyAuraEffect(SPELLFAMILY_SHAMAN, 2289, 0))
-                            {
                                 if (roll_chance_i(aur->GetBaseAmount()) && target->HasAuraWithMechanic(1 << MECHANIC_SNARE))
-                                {
                                     caster->CastSpell(caster, SHAMAN_TOTEM_SPELL_EARTHEN_POWER, true, NULL, aurEff);
-                                    caster->CastSpell(caster, SHAMAN_TOTEM_SPELL_EARTHEN_POWER_BUFF, true, NULL, aurEff);
-                                }
-                            }
-                        }
-                    }
-                }
             }
 
             void Register()
