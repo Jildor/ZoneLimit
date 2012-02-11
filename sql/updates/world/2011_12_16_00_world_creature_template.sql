7e33cd6d8ff127d0cc7fb59fc4655bcb0e1ccce5
Jildor <gildor55@gmail.com>
2012-02-11 11:55:05 +0100

    Core/Spells: Added spells for Fire Elemental Totem and Earth Elemental Totem
    https://github.com/TrinityCore/TrinityCore/commit/eeb5a2d95efdc37a9b814bc653005e71930c2092

---
+++ sql/updates/world/2011_12_16_00_world_creature_template.sql
@@ -0,0 +1,6 @@
+-- Editado por Gildor
+-- Los pongo como en TDB
+-- Greater Fire Elemental script
+UPDATE `creature_template` SET `faction_A`=1629,`faction_H`=1629,`ScriptName`='npc_fire_elemental' WHERE `entry`=15438;
+-- Greater Earth Elemental script
+UPDATE `creature_template` SET `faction_A`=1629,`faction_H`=1629,`ScriptName`='npc_earth_elemental' WHERE `entry`=15352;
