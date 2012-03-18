1483662c0399685aef9151cf571244158bea4c88
Jildor <gildor55@gmail.com>
2012-03-18 17:32:57 +0100

    testing Spinning Pain Spike damage with spellscript

---
+++ sql/updates/world/spinning_pain_spike.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (66316);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES 
+(66316,'spell_spinning_pain_spike');
--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_lord_jaraxxus.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_lord_jaraxxus.cpp
@@ -527,6 +527,41 @@ public:
 
 };
 
+class spell_spinning_pain_spike : public SpellScriptLoader
+{
+    enum Spells
+    {
+        SPELL_SPINNING_PAIN_SPIKE = 66316,
+    };
+
+    public:
+        spell_spinning_pain_spike() : SpellScriptLoader("spell_spinning_pain_spike") {}
+
+        class spell_spinning_pain_spike_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_spinning_pain_spike_SpellScript);
+
+            void HandleScript(SpellEffIndex /*eff*/)
+            {
+                int32 bp = 0;
+                if (Unit* unitTarget = GetHitUnit())
+                {
+                    bp = int32(unitTarget->CountPctFromMaxHealth(50));
+                    unitTarget->CastCustomSpell(unitTarget, SPELL_SPINNING_PAIN_SPIKE, &bp, NULL, NULL, false);
+                }
+            }
+            void Register()
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_spinning_pain_spike_SpellScript::HandleScript, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_spinning_pain_spike_SpellScript();
+        }
+};
+
 void AddSC_boss_jaraxxus()
 {
     new boss_jaraxxus();
@@ -535,4 +570,5 @@ void AddSC_boss_jaraxxus()
     new mob_fel_infernal();
     new mob_nether_portal();
     new mob_mistress_of_pain();
+    new spell_spinning_pain_spike();
 }
