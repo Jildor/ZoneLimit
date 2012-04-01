f5fa43c2c198fa5ba31c3050c6335fa30b8aa969
Jildor <gildor55@gmail.com>
2012-04-01 17:38:54 +0200

    testing Spinning Pain Spike damage with spellscript

---
+++ sql/updates/world/spinning_pain_spike.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (66316,67100,67101,67102);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(66316,'spell_spinning_pain_spike'),
+(67100,'spell_spinning_pain_spike'),
+(67101,'spell_spinning_pain_spike'),
+(67102,'spell_spinning_pain_spike'),
