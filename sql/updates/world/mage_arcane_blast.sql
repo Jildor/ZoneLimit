6ed654aee6a5af17ed6cda097ea98a0ddb0a14a5
Jildor <gildor55@gmail.com>
2012-04-15 18:05:54 +0200

    testing

---
+++ sql/updates/world/mage_arcane_blast.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_mage_arcane_blast';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(30451, 'spell_mage_arcane_blast');
