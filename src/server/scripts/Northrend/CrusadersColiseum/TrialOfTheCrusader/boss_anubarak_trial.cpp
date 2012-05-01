d38098a0611edd6f08160f14bdf92baf502e6e79
Jildor <gildor55@gmail.com>
2012-05-01 16:57:39 +0200

    testing

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_anubarak_trial.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_anubarak_trial.cpp
@@ -681,12 +681,13 @@ public:
             m_uiSpeed = 0;
             m_uiIncreaseSpeedTimer = 1*IN_MILLISECONDS;
 
-            if (!who || !who->isAlive() || !who->HasAura(SPELL_MARK))
-            return;
+            if (who || who->isAlive() || who->HasAura(SPELL_MARK))
+            {
+                AttackStart(who);
+                me->AddThreat(who, 10000000.0f);
+                me->TauntApply(who);
+            }
 
-            AttackStart(who);
-            me->AddThreat(who, 10000000.0f);
-            me->TauntApply(who);
         }
 
         void DamageTaken(Unit* /*who*/, uint32& uiDamage)
