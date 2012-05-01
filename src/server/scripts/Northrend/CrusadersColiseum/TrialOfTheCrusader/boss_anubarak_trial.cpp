d629fbc14b0b92c137f2d20c43cdfaef3412f95c
Jildor <gildor55@gmail.com>
2012-05-01 19:22:05 +0200

    Scripts/ToCr: * Temp fix spike anub agro.

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_anubarak_trial.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_anubarak_trial.cpp
@@ -648,6 +648,10 @@ public:
         mob_anubarak_spikeAI(Creature* creature) : ScriptedAI(creature)
         {
             m_instance = (InstanceScript*)creature->GetInstanceScript();
+            me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_TAUNT, true);
+            me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_ATTACK_ME, true);
+            me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK, true);
+            me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_GRIP, true);
         }
 
         InstanceScript* m_instance;
@@ -664,9 +668,7 @@ public:
 
         void MoveInLineOfSight(Unit* who)
         {
-
             who = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0F, true, 0);
-            AttackStart(who);
         }
 
         void EnterCombat(Unit* who)
@@ -677,6 +679,8 @@ public:
             m_uiSpeed = 0;
             m_uiIncreaseSpeedTimer = 1*IN_MILLISECONDS;
             me->TauntApply(who);
+            me->AddThreat(who, 10000000.0f);
+            AttackStart(who);
         }
 
         void DamageTaken(Unit* /*who*/, uint32& uiDamage)
