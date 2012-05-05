90c3c5bfcbbd93c82951339567b250aa2f1b0ea9
Jildor <gildor55@gmail.com>
2012-05-05 13:08:21 +0200

    Core-SQL/Spells: Revert Fix wyvern sting, crash server.

---
+++ sql/updates/world/hunter_wyvern_sting.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_linked_spell` WHERE `comment` = 'Wyvern Sting';
+INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
+(-19386,24131,0,'Wyvern Sting'),
+(-24132,24134,0,'Wyvern Sting'),
+(-24133,24135,0,'Wyvern Sting'),
+(-27068,27069,0,'Wyvern Sting'),
+(-49011,49009,0,'Wyvern Sting'),
+(-49012,49010,0,'Wyvern Sting');
