3ca28eb3ccca73485bece07c31b13d2737e73643
Jildor <gildor55@gmail.com>
2012-07-05 21:07:24 +0200

    typo

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -1249,7 +1249,7 @@ FakeResult Item::SetFakeDisplay(uint32 iEntry)
 
     ItemTemplate const* myTmpl    = GetTemplate();
     ItemTemplate const* otherTmpl = sObjectMgr->GetItemTemplate(iEntry);
-    Player const* player = ObjectAccessor::FindPlayer(*itr);
+    Player const* player = ObjectAccessor::FindPlayer(GetOwnerGUID());
 
     if (!otherTmpl)
         return FAKE_ERR_CANT_FIND_ITEM;
