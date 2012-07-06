d75c7c1ee30f8ad415090b459b3c256291f76064
Jildor <gildor55@gmail.com>
2012-07-06 17:27:06 +0200

    testing

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -1254,7 +1254,7 @@ FakeResult Item::SetFakeDisplay(uint32 iEntry)
     // Item const* pItem             = player->GetItemByPos();
     ItemTemplate const* myTmpl    = GetTemplate();
     ItemTemplate const* otherTmpl = sObjectMgr->GetItemTemplate(iEntry);
-    // Player const* player;
+    Player const* player = _player;
 
 
     if (!otherTmpl)
