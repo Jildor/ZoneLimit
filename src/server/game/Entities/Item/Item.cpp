e3a4c08ee537912ccffa257173fd9abd6b0fdf3f
Jildor <gildor55@gmail.com>
2012-07-06 16:48:07 +0200

    testing

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -1269,7 +1269,7 @@ FakeResult Item::SetFakeDisplay(uint32 iEntry)
     if (otherTmpl->Quality == ITEM_QUALITY_LEGENDARY || otherTmpl->Quality == ITEM_QUALITY_POOR)
         return FAKE_ERR_WRONG_QUALITY;
 
-    if(player->CanUseItem(myTmpl) == EQUIP_ERR_OK)
+    if(player->CanUseItem(myTmpl, not_loading) == EQUIP_ERR_OK)
     {
         uint32 NClass = myTmpl->Class;
         uint32 OClass = otherTmpl->Class;
