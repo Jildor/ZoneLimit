dfe86b7f5c62e0dbc6e0e8a590c33209d460eb5d
Jildor <gildor55@gmail.com>
2012-07-13 18:04:52 +0200

    testing

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -467,8 +467,8 @@ bool Item::LoadFromDB(uint32 guid, uint64 owner_guid, Field* fields, uint32 entr
     SetUInt32Value(ITEM_FIELD_CREATE_PLAYED_TIME, fields[9].GetUInt32());
     SetText(fields[10].GetString());
 
-    if (QueryResult result = CharacterDatabase.PQuery("SELECT fakeEntry FROM fake_items WHERE guid = %u", guid))
-        SetFakeDisplay((*result)[0].GetUInt32());
+    if (uint32 fakeEntry = sObjectMgr->GetFakeItemEntry(guid))
+        SetFakeDisplay(fakeEntry);
 
     if (need_save)                                           // normal item changed state set not work at loading
     {
@@ -486,7 +486,7 @@ bool Item::LoadFromDB(uint32 guid, uint64 owner_guid, Field* fields, uint32 entr
 /*static*/
 void Item::DeleteFromDB(SQLTransaction& trans, uint32 itemGuid)
 {
-    // sObjectMgr->RemoveFakeItem(itemGuid);
+    sObjectMgr->RemoveFakeItem(itemGuid);
     PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_ITEM_INSTANCE);
     stmt->setUInt32(0, itemGuid);
     trans->Append(stmt);
@@ -1363,6 +1363,7 @@ void Item::RemoveFakeDisplay()
     if (GetFakeDisplayEntry())
     {
         m_fakeDisplayEntry = 0;
+        sObjectMgr->RemoveFakeItem(GetGUIDLow());
         CharacterDatabase.PExecute("DELETE FROM fake_items WHERE guid = %u", GetGUIDLow());
     }
 }
