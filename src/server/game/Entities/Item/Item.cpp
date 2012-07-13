4f33b50f509bd93ae1f126b0f690aae5e97778fb
Jildor <gildor55@gmail.com>
2012-07-13 17:45:13 +0200

    testing

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -487,7 +487,7 @@ bool Item::LoadFromDB(uint32 guid, uint64 owner_guid, Field* fields, uint32 entr
 void Item::DeleteFromDB(SQLTransaction& trans, uint32 itemGuid)
 {
     sObjectMgr->RemoveFakeItem(itemGuid);
-    RemoveFakeDisplay();
+    RemoveFakeDisplay(itemGuid);
     PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_ITEM_INSTANCE);
     stmt->setUInt32(0, itemGuid);
     trans->Append(stmt);
