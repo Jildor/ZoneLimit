eca8af2004265a3c751fbd62af0f6526c6dce21b
Jildor <gildor55@gmail.com>
2012-07-18 12:59:53 +0200

    testing

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -2707,11 +2707,8 @@ void ObjectMgr::RemoveFakeItem(uint32 itemGuid)
 {
     FakeItemsMap::iterator itr = fakeItemsStore.find(itemGuid);
     if (itr != fakeItemsStore.end())
-    {
         fakeItemsStore.erase(itr);
-    /*sObjectMgr->fakeItemsStore.erase(itemGuid);*/
-    CharacterDatabase.PExecute("DELETE FROM fake_items WHERE guid = %u", itr);
-    }
+    CharacterDatabase.PExecute("DELETE FROM fake_items WHERE guid = %u", itemGuid);
 }
 
 void ObjectMgr::LoadItemSetNameLocales()
