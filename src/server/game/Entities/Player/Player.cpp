a77da12cf16c318574050db16f2f02ed7781d61d
Jildor <gildor55@gmail.com>
2012-07-18 11:40:17 +0200

    typo

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -12407,7 +12407,7 @@ void Player::MoveItemFromInventory(uint8 bag, uint8 slot, bool update)
 {
     if (Item* it = GetItemByPos(bag, slot))
     {
-        it->DeleteFakeFromDB(it->GetGUIDLow());
+        it->RemoveFakeDisplay(it->GetGUIDLow());
         ItemRemovedQuestCheck(it->GetEntry(), it->GetCount());
         RemoveItem(bag, slot, update);
         it->SetNotRefundable(this, false);
