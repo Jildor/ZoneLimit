d3173e1c03c87c28b4c72356d4e97519522e0189
Jildor <gildor55@gmail.com>
2012-08-03 13:13:43 +0200

    Core/Quests: Script quest Captain Tyralius.
    https://github.com/TrinityCore/TrinityCore/commit/c3e527da6d0e5b6fcf7c7faadfd441c2f0a941bc

---
+++ sql/updates/world/2011_12_25_00_world_misc.sql
@@ -0,0 +1,7 @@
+-- Editado por Gildor
+UPDATE `gameobject_template` SET `ScriptName`='go_captain_tyralius_prison' WHERE `entry`=184588;
+-- Le pongo reqnpc como en TDB
+UPDATE `quest_template` SET `RequiredNpcOrGo1`=20787,`ReqSpellCast1`=0 WHERE `entry`=10422;
+DELETE FROM `creature_text` WHERE `entry`=20787;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(20787,0,0,"You've saved me, fleshling! Ameer will hear of this noble act.",12,0,100,0,0,0,"Captain Tyralius");
