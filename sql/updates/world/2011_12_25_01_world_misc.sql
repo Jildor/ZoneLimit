9d4c711ccda034485c72ccbec0fb304ff1836471
Jildor <gildor55@gmail.com>
2012-08-03 15:45:45 +0200

    Scripts/Quests: Zuhuled the Wacked And obviously, some typo's.
    https://github.com/TrinityCore/TrinityCore/commit/49e10f992a7e73e4afcc627cfbdf0dc3db28ef1a

---
+++ sql/updates/world/2011_12_25_01_world_misc.sql
@@ -0,0 +1,5 @@
+-- Editado por Gildor
+-- Zuhuled the Wacked
+-- Le pongo reqcre1 como en TDB
+UPDATE `quest_template` SET `RequiredNpcOrGo1`=22112,`StartScript`=0,`CompleteScript`=0 WHERE `id`=10866;
+DELETE FROM `quest_start_scripts` WHERE `id`=10866;
