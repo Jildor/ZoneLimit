748fb44e71808999b6089e394a775030f361eec6
Jildor <gildor55@gmail.com>
2012-08-04 16:33:25 +0200

    Typo fix by higi
    https://github.com/TrinityCore/TrinityCore/commit/65a427a3226b1ff1ec9431170ceb8f5e1747e3bb

--- src/server/game/Chat/Chat.cpp
+++ src/server/game/Chat/Chat.cpp
@@ -897,11 +897,11 @@ bool ChatHandler::ShowHelpForSubCommands(ChatCommand* table, char const* cmd, ch
     std::string list;
     for (uint32 i = 0; table[i].Name != NULL; ++i)
     {
-        // must be available (ignore handler existence for show command with possibe avalable subcomands
+        // must be available (ignore handler existence for show command with possible avalable subcomands)
         if (!isAvailable(table[i]))
             continue;
 
-        /// for empty subcmd show all available
+        // for empty subcmd show all available
         if (*subcmd && !hasStringAbbr(table[i].Name, subcmd))
             continue;
 
@@ -936,7 +936,7 @@ bool ChatHandler::ShowHelpForCommand(ChatCommand* table, const char* cmd)
     {
         for (uint32 i = 0; table[i].Name != NULL; ++i)
         {
-            // must be available (ignore handler existence for show command with possibe avalable subcomands
+            // must be available (ignore handler existence for show command with possible avalable subcomands)
             if (!isAvailable(table[i]))
                 continue;
 
@@ -966,7 +966,7 @@ bool ChatHandler::ShowHelpForCommand(ChatCommand* table, const char* cmd)
     {
         for (uint32 i = 0; table[i].Name != NULL; ++i)
         {
-            // must be available (ignore handler existence for show command with possibe avalable subcomands
+            // must be available (ignore handler existence for show command with possible avalable subcomands)
             if (!isAvailable(table[i]))
                 continue;
 
